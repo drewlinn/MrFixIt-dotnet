8e70549b1976a4e05c1d1812981512505fb3333b
Andrew <expandrew@gmail.com>
2017-08-28 19:48:27 -0700

    work on building CRUD functionality for our Job and Worker objects.

--- src/MrFixIt/Controllers/JobsController.cs
+++ src/MrFixIt/Controllers/JobsController.cs
@@ -45,7 +45,7 @@ namespace MrFixIt.Controllers
             job.Worker = db.Workers.FirstOrDefault(i => i.UserName == User.Identity.Name);
             db.Entry(job).State = EntityState.Modified;
             db.SaveChanges();
-            return Content("Claimed by" + job.Worker.FirstName + " " + job.Worker.LastName, "text/plain");
+            return Content("Claimed by " + job.Worker.FirstName + " " + job.Worker.LastName, "text/plain");
         }
 
         //public IActionResult Claim(Job job)
